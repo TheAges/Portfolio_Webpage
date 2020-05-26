ad4251bff2edffdedd7e690bebe13b2df0eeb29f
TheAges <dragoneage@gmail.com>
2020-05-26 16:04:06 +0200

    Add files via upload

--- js/mainJS.js
+++ js/mainJS.js
@@ -17,6 +17,15 @@ tapGrid6 = 0,
 tapGrid7 = 0,
 tapGrid8 = 0;
 
+var isMobile = false; //initiate as false
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+    isMobile = true;
+}
+
+var isFirefox = false;
+if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){isFirefox = true;}
 //On hash change and On load
 
 function changePage(position) {
@@ -30,7 +39,7 @@ function changePage(position) {
 };
 
 window.onhashchange = function(){
-  if (animationBusy!=true) changePage(window.location.hash);
+  if ((animationBusy!=true)||(isFirefox)) changePage(window.location.hash);
   console.log("window hash changed")
 };
 
@@ -38,6 +47,10 @@ window.onload = function () {
   if (window.location.hash!="") {
     skipShowDown();
     changePage(window.location.hash);
+  }
+  if (isMobile && !skipIntro) {
+    document.querySelector("#videoCover").remove();
+    document.querySelector("#wrapperCover").innerHTML = '<img src="res/Images/bgStill.png" class="slowRotation_fadeIn col-12 col-md-7 col-lg-6"><div id="coverQuote" style="margin-top: 15px;" class="col-12 coverQuoteHover fadeInQuote">"Design, like creativity, is a process that go <br> from chaos to order"</div>'
   }
 };
 
@@ -93,8 +106,12 @@ function triggerShowOff() {
 
   if (isCoverVideoFinish) {showDown();};
 
-  document.getElementById("videoCover").playbackRate = playbackSpeed;
-  console.log(document.getElementById("videoCover").playbackRate);
+  if (!isMobile) {
+    document.getElementById("videoCover").playbackRate = playbackSpeed;
+    console.log(document.getElementById("videoCover").playbackRate);
+  }
+  else {showDown();}
+
 }
 
 document.getElementById('videoCover').addEventListener('ended',videoOver,false);
@@ -306,6 +323,7 @@ function goToSingleWork(path) {
     animationBusy = true;
     window.location.hash=path;
 
+    $("#workWrapper").empty();
     $("#workWrapper").load( path+" #workContenet" );
 
     $("#containerTitle").css("opacity","0");
@@ -346,6 +364,7 @@ function goToSingleWork(path) {
       left: 0,
     });
 
+    $("#workWrapper").empty();
     $("#workWrapper").load( path+" #workContenet" );
 
     $("#worksGrid").addClass("hide");
@@ -386,6 +405,7 @@ function backWorks() {
       $("#worksGrid").css('opacity', '0');
 
       timeouts.push(setTimeout(function() {
+
         $("#about").addClass("hide");
         $("#workWrapper").addClass("hide");
         $("#backTop").addClass("hide");
@@ -485,7 +505,7 @@ else {
     tapGrid7=0;
     tapGrid8=0;
 
-    if (tapGrid1>1) {goToSingleWork("content/To_Anagrafe.html");}
+    if (tapGrid1>1) {goToSingleWork("content/To_Anagrafe.html"); tapGrid1=0}
   });
 
   $("#gridItem-BlaBlaCar_UX").click(function() {
@@ -498,7 +518,7 @@ else {
     tapGrid7=0;
     tapGrid8=0;
 
-    if (tapGrid2>1) {goToSingleWork('content/BlaBlaCar_UX.html');}
+    if (tapGrid2>1) {goToSingleWork('content/BlaBlaCar_UX.html'); tapGrid2=0}
   });
 
   $("#gridItem-Trenord_Redesig").click(function() {
@@ -511,7 +531,7 @@ else {
     tapGrid7=0;
     tapGrid8=0;
 
-    if (tapGrid3>1) {goToSingleWork('content/Trenord_Redesign.html');}
+    if (tapGrid3>1) {goToSingleWork('content/Trenord_Redesign.html'); tapGrid3=0}
   });
 
   $("#gridItem-Il_Nemico").click(function() {
@@ -524,7 +544,7 @@ else {
     tapGrid7=0;
     tapGrid8=0;
 
-    if (tapGrid4>1) {goToSingleWork('content/Il_Nemico.html');}
+    if (tapGrid4>1) {goToSingleWork('content/Il_Nemico.html'); tapGrid4=0}
   });
 
   $("#gridItem-Cretive_Coding").click(function() {
@@ -537,7 +557,7 @@ else {
     tapGrid7=0;
     tapGrid8=0;
 
-    if (tapGrid5>1) {goToSingleWork('content/Cretive_Coding.html');}
+    if (tapGrid5>1) {goToSingleWork('content/Cretive_Coding.html'); tapGrid5=0}
   });
 
   $("#gridItem-AV_Trust").click(function() {
@@ -550,7 +570,7 @@ else {
     tapGrid7=0;
     tapGrid8=0;
 
-    if (tapGrid6>1) {goToSingleWork('content/AV_Trust.html');}
+    if (tapGrid6>1) {goToSingleWork('content/AV_Trust.html'); tapGrid6=0}
   });
 
   $("#gridItem-StraightUp").click(function() {
@@ -563,7 +583,7 @@ else {
     tapGrid7++;
     tapGrid8=0;
 
-    if (tapGrid7>1) {goToSingleWork('content/StraightUp.html');}
+    if (tapGrid7>1) {goToSingleWork('content/StraightUp.html'); tapGrid7=0}
   });
 
   $("#gridItem-Ansiocromo").click(function() {
@@ -576,7 +596,7 @@ else {
     tapGrid7=0;
     tapGrid8++;
 
-    if (tapGrid8>1) {goToSingleWork('content/Ansiocromo.html');}
+    if (tapGrid8>1) {goToSingleWork('content/Ansiocromo.html'); tapGrid8=0}
   });
 }
 
@@ -592,11 +612,11 @@ function changeElementsOnDimension() {
     document.querySelector("#gridItem-Trenord_Redesig").parentElement.removeAttribute("style");
   };
 
-  if (window.innerWidth > 768) {$("#StrightUp-2_Img").attr("src", "../res/Images/StraightUp/02.1.svg");console.log("AAA")}
-  else if ((window.innerWidth > 400) && (window.innerWidth < 768)) {$("#StrightUp-2_Img").attr("src", "../res/Images/StraightUp/02.2.svg");console.log("AAA")}
-  else {$("#StrightUp-2_Img").attr("src", "../res/Images/StraightUp/02.3.svg");console.log("AAA")};
+  if (window.innerWidth > 768) {$("#StrightUp-2_Img").attr("src", "../res/Images/StraightUp/02.1.svg");}
+  else if ((window.innerWidth > 400) && (window.innerWidth < 768)) {$("#StrightUp-2_Img").attr("src", "../res/Images/StraightUp/02.2.svg");}
+  else {$("#StrightUp-2_Img").attr("src", "../res/Images/StraightUp/02.3.svg");};
 
-  if (window.innerWidth > 1920) {
+  if (window.innerWidth > 1928) {
     $(".container-fluid").addClass("container");
     $(".container-fluid").removeClass("container-fluid");
   }
